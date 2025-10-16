58e2f07424f06f781b7c9004dd1e6554efcf13c0
treeder <treeder@gmail.com>
2025-10-16 05:44:48 +0000

    Simplify dialog and divider

--- divider/divider.js
+++ divider/divider.js
@@ -3,20 +3,60 @@
  * Copyright 2023 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
-import { Divider } from './internal/divider.js';
-import { styles } from './internal/divider-styles.js';
+import { LitElement, css } from 'lit'
 /**
- * @summary A divider is a thin line that groups content in lists and
- * containers.
- *
- * @description Dividers can reinforce tapability, such as when used to separate
- * list items or define tappable regions in an accordion.
- *
- * @final
- * @suppress {visibility}
+ * A divider component.
  */
-export let MdDivider = class MdDivider extends Divider {
-};
-MdDivider.styles = [styles];
-
-customElements.define('md-divider', MdDivider)
+export class Divider extends LitElement {
+  static properties = {
+    inset: { type: Boolean, reflect: true },
+    insetStart: { type: Boolean, reflect: true, attribute: 'inset-start' },
+    insetEnd: { type: Boolean, reflect: true, attribute: 'inset-end' },
+  }
+  constructor() {
+    super(...arguments)
+    /**
+     * Indents the divider with equal padding on both sides.
+     */
+    this.inset = false
+    /**
+     * Indents the divider with padding on the leading side.
+     */
+    this.insetStart = false
+    /**
+     * Indents the divider with padding on the trailing side.
+     */
+    this.insetEnd = false
+  }
+  static styles = [
+    css`
+      :host {
+        box-sizing: border-box;
+        color: var(--md-divider-color, var(--md-sys-color-outline-variant, #cac4d0));
+        display: flex;
+        height: var(--md-divider-thickness, 1px);
+        width: 100%;
+      }
+      :host([inset]),
+      :host([inset-start]) {
+        padding-inline-start: 16px;
+      }
+      :host([inset]),
+      :host([inset-end]) {
+        padding-inline-end: 16px;
+      }
+      :host::before {
+        background: currentColor;
+        content: '';
+        height: 100%;
+        width: 100%;
+      }
+      @media (forced-colors: active) {
+        :host::before {
+          background: CanvasText;
+        }
+      }
+    `,
+  ]
+}
+customElements.define('md-divider', Divider)
